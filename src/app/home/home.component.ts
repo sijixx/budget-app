4b4fa1af4b148ba01ade292289ce33f04235d16e
sijixxx <awofalasiji@gmail.com>
2023-04-12 01:55:59 +0100

    worked on expense display and sorted by latest

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,4 @@
-import { Component } from '@angular/core';
-
-
-interface Expense {
-  name: string;
-  price: number;
-  date: string;
-  mode: string;
-  narration: string;
-}
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,19 +6,66 @@ interface Expense {
 })
 
 
-export class HomeComponent {
-  newExpense: Expense = {
+export class HomeComponent implements OnInit {
+  displayform = false;
+
+  newExpense: any = {
     name: '',
     price: 0,
     date: '',
     mode: '',
     narration: ''
-};
+  };
 
-expenses: Expense[] = [];
+  expenses: any = [];
 
-addExpense() {
+  constructor(){}
+
+  ngOnInit(): void {
+    this.getexpenses();
+  }
+
+  getexpenses() {
+
+    
+
+    const expensesString = localStorage.getItem('expense');
+    if (expensesString) {
+      this.expenses = JSON.parse(expensesString).reverse();
+    } else {
+      this.expenses = [];
+    }
+  }
+
+  addExpense() {
+    const currentExpenses = JSON.parse(localStorage.getItem('expense') || '[]');
+    currentExpenses.push(this.newExpense);
+    localStorage.setItem('expense', JSON.stringify(currentExpenses));
+
+    this.getexpenses();
+    this.clearform();
+    
+  }
+
+  clearform(){
+    this.newExpense = {
+      name: '',
+      price: 0,
+      date: '',
+      mode: '',
+      narration: ''
+    };
+
+    this.displayform = false;
+  }
+
+  showform(){
+    this.displayform = true;
+  }
+
+  hideform(){
+    this.displayform = false;
+  }
   
-}
 }
 
